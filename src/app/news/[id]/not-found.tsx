96d8ed2804904bce458b74d508bca70c83662e73
Mojeeto <mojtaba01nouri@gmail.com>
2024-05-12 15:33:35 +0330

    [feat] notfound page

---
+++ src/app/news/[id]/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NewsNotFoundPage() {
+  return (
+    <div className="error">
+      <h1>Not Found!</h1>
+      <p>Unfortunately, we could not find the request article.</p>
+    </div>
+  );
+}
