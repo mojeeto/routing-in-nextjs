2662c3b0754c0e159a210a0ac3a5fdcdbb57a7bf
Mojeeto <mojtaba01nouri@gmail.com>
2024-05-16 14:11:22 +0330

    [dev] client-side data fetching

--- src/app/(content)/news/page.tsx
+++ src/app/(content)/news/page.tsx
@@ -1,11 +1,49 @@
-import { DUMMY_NEWS } from "@/dummy-news";
+"use client";
 import NewsList from "@/components/news-list";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+  const [news, setNews] = useState<
+    { id: string; slug: string; image: string; title: string }[]
+  >([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      const response = await fetch("http://localhost:8080/news");
+
+      if (!response.ok) {
+        setLoading(false);
+        setError("Failed to fetch news.");
+      }
+
+      const news = await response.json();
+      setLoading(false);
+      setNews(news);
+    }
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  let newsContent;
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 }
